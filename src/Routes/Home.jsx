3f9729197a470c8082c305e50db7a94c74d80808
javlav98 <jrlavin98@gmail.com>
2024-02-08 10:53:32 -0600

    working on bg

--- src/Routes/Home.jsx
+++ src/Routes/Home.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import './Home.css'
 
 export default function Home() {
   return (
-    <div>
-      <h1 className='titlePage'>Home</h1>
+    <div className='homePage'>
+      <h1 className='title'>Making imagtions become a visual reality </h1>
+      { /* i want to create a contact button*/}
+      <button className='contactButton'>Contact</button>
+
       
     </div>
   )
